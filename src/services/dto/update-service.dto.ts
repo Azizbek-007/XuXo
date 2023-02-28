e53cffa46e8d03194ca1b7143d21ea2dc2bd72d4
Azizbek Berdimuratov <77209269+Azizbek-007@users.noreply.github.com>
2023-02-28 15:25:29 +0500

    commix

--- src/services/dto/update-service.dto.ts
+++ src/services/dto/update-service.dto.ts
@@ -2,3 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateServiceDto } from './create-service.dto';
 
 export class UpdateServiceDto extends PartialType(CreateServiceDto) {}
+ 
