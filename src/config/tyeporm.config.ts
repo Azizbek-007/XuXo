5a9c2150fc40732c849ca9f74bab69655d9906eb
Azizbek Berdimuratov <77209269+Azizbek-007@users.noreply.github.com>
2023-03-09 23:13:20 +0500

    deploy

--- src/config/tyeporm.config.ts
+++ src/config/tyeporm.config.ts
@@ -11,7 +11,6 @@ export const TypeOrmConfig: TypeOrmModuleOptions = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: entities,
-  // autoLoadEntities: true,
   synchronize: true,
   logging: false
 };  
